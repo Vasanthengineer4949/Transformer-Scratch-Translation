ed245774a46fd22fc86e533fbeb0222d60feee37
Vasanth P <vasanthp.cse2019@citchennai.net>
2023-11-03 14:35:50 +0530

    Embedding Updated

--- src/embedding.py
+++ src/embedding.py
@@ -1,41 +1,66 @@
-from config import *
 import torch
 import torch.nn as nn
 
-class TokenEmbedding(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-        self.d_model = D_MODEL
-        self.vocab_size = VOCAB_SIZE
-        self.embedding = nn.Embedding( # Embedding layer of shape -> (vocab_size, d_model)
-            num_embeddings=self.vocab_size,
-            embedding_dim=self.d_model
-        )
+class TokenEmbeddings(nn.Module):
     
-    def forward(self, x):
-        x = self.embedding(x) # Create token embeddings
-        return x
+    def __init__(self, d_model: int, vocab_size: int) -> nn.Embedding:
+        
+        '''
+        Computes the token embeddings for input tokens
+        
+        Args:
+        d_model - Input representation(Embedding) dimension
+        vocab_size - Size of vocabulary
+        
+        Returns:
+        token_embedding - Embedding of Input Tokens
+        '''
+        
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(
+                                        num_embeddings=vocab_size,
+                                        embedding_dim=d_model
+                                    ) # Shape: (vocab_size, embed_dim)
+        
+    def forward(self, x: torch.Tensor):
+        token_embedding = self.embedding(x) # Embedding created using Embedding class of Torch
+        return token_embedding
     
 class PositionalEncoding(nn.Module):
-
-    def __init__(self):
-        self.d_model = D_MODEL 
-        self.seq_len = MAX_SEQ_LEN
-        self.dropout_p = DROPOUT_P
-        self.dropout = nn.Dropout(self.dropout_p) # Dropout layer
-        self.position_encodings = torch.zeros(self.seq_len, self.d_model) # Shape: (seq_len, d_model)
-        self.positions = torch.arange(0, self.seq_len, dtype=torch.float).unsqueeze(0) # Shape: (seq_len) -> (seq_len, 1)
-        self.even_odd_i = torch.arange(0, self.d_model, 2).float() # 2i # Shape: (d_model/2)
-        self.div_freqs_term = torch.pow(10000, self.even_odd_i/self.d_model) # 10000**2i/dmodel
-        self.position_encodings[:, 0::2] = torch.sin(self.positions*self.div_freqs_term) # Shape: (seq_len, d_model)
-        self.position_encodings[:, 1::2] = torch.cos(self.positions*self.div_freqs_term) # Shape: (seq_len, d_model)
-        self.position_encodings = self.position_encodings.unsqueeze(0) # Shape: (1, seq_len, d_model)
-        self.register_buffer(self.position_encodings) # to be a part of module state but not a parameter of module
-
-    def forward(self, x):
-        x = x + (self.position_encodings[:, :x.shape[1], :]) # Adding positional encodings
-        x = self.dropout(x)
-        return x
-
+    
+    def __init__(self, d_model: int, seq_len: int, dropout_p: float) -> None:
         
+        '''
+        Computes the Positional Encoding for the Input Tokens positions
+        
+        Args:
+        d_model - Embedding dimension
+        seq_len - Number of tokens in the input sequence
+        dropout - Dropout value
+        
+        Returns:
+        pos_encoding - Encoded position values
+        '''
+        
+        super().__init__()
+        
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout_p = dropout_p
+        
+        self.dropout = nn.Dropout(self.dropout_p) # Dropout layer
+        position_encodings = torch.zeros(self.seq_len, self.d_model) # Shape: (seq_len, d_model)
+        positions = torch.arange(0, self.seq_len, dtype=torch.float).unsqueeze(0) # Shape: (seq_len) -> (seq_len, 1)
+        even_odd_i = torch.arange(0, self.d_model, 2).float() # 2i # Shape: (d_model/2)
+        div_freqs_term = torch.pow(10000, even_odd_i/d_model) # 10000**2i/dmodel
+        position_encodings[:, 0::2] = torch.sin(positions*div_freqs_term) # Shape: (seq_len, d_model)
+        position_encodings[:, 1::2] = torch.cos(positions*div_freqs_term) # Shape: (seq_len, d_model)
+        position_encodings = position_encodings.unsqueeze(0) # Shape: (1, seq_len, d_model)
+        self.register_buffer(position_encodings) # to be a part of module state but not a parameter of module
+        
+    def forward(self, x):
+        x = x + (self.position_encodings[:, :x.shape[1], :]).requires_grad_(False) # Adding positional encodings
+        pos_encoding = self.dropout(x)
+        return pos_encoding
