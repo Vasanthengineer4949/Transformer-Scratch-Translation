6ea45a5185b81f30e320d0d70d55defbf1bdc54f
Vasanth <vasanthp.cse2019@citchennai.net>
2023-12-10 06:02:21 +0530

    FeedForward Block Completed

--- src/config.py
+++ src/config.py
@@ -3,3 +3,4 @@ VOCAB_SIZE = 10000 # Number of tokens in vocabulary
 MAX_SEQ_LEN = 128 # Maximum Sequence Length of Input and Output Sequence
 DROPOUT_P = 0.1 # Quantify the dropout of how much
 EPS = 1e-6 # A small value to avoid zero division error while normalization
+FF_DROPOUT = 0.2 # How much dropout in FFN
