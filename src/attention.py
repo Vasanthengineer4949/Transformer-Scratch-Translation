3e75851ec79cba66d68e65ba96f37ca0b56b336f
Vasanth <vasanthp.cse2019@citchennai.net>
2023-12-12 10:12:09 +0530

    Attention Block Completed

--- src/attention.py
+++ src/attention.py
@@ -16,16 +16,21 @@ class MultiHeadAttentionBlock(nn.Module):
 
     @staticmethod
     def attention(query, key, value, mask, dropout: nn.Dropout):
+
         head_dim = query.shape[-1]
+        
         # Just apply the formula from the paper
         # (bs, num_heads, seq_len, head_dim) --> (bs, num_heads, seq_len, seq_len)
         attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(head_dim)
+
         if mask is not None:
             # Write a very low value (indicating -inf) to the positions where mask == 0
             attention_scores.masked_fill_(mask == 0, -1e9)
         attention_scores = attention_scores.softmax(dim=-1) # (bs, num_heads, seq_len, seq_len) # Apply softmax
+
         if dropout is not None:
             attention_scores = dropout(attention_scores)
+
         # (bs, h, seq_len, seq_len) --> (bs, h, seq_len, head_dim)
         # return attention scores which can be used for visualization
         return (attention_scores @ value), attention_scores
@@ -46,7 +51,7 @@ class MultiHeadAttentionBlock(nn.Module):
         # Calculate attention
         x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, attn_mask, self.dropout)
         
-        # Combine all the heads together
+        # Concatenate all the heads together
         # (bs, num_heads, seq_len, head_dim) --> (bs, seq_len, num_heads, head_dim) --> (bs, seq_len, d_model)
         x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.num_heads * self.head_dim)
 
